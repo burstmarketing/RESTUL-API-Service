0b6940014965992d8c10eea676b66b4656088045
Christopher Kotfila <kotfic@gmail.com>
2012-03-17 23:34:21 -0400

    adding getActiveTickets action

--- App/Assembla/Controller/Custom.php
+++ App/Assembla/Controller/Custom.php
@@ -4,6 +4,17 @@ class Assembla_Controller_Custom extends Assembla_Controller_Abstract {
   public function getConfig(){
 	return file_get_contents( RESTFUL_API_LOADER::getBaseDir() . "/Assembla/etc/config.json");
   }
+
+  public function getActiveTickets(){
+	$_spaces = $this->_api->loadMySpacesList();
+	foreach( $_spaces AS $_space ){
+	  $_space->setActiveTickets( $this->_api->loadTicketReport( array( "report_id" => 8, 
+																	   "space_id" => $_space->getId())) );
+	}
+
+	return $_spaces->toJSON();
+	
+  }
   
   }
 
